68328c8170a66af7ed463ed09821c135710f7f41
Sergey <jade.armadillo@gmail.com>
2010-06-03 17:12:39 +0300

    page layout (css)

--- urls.py
+++ urls.py
@@ -8,3 +8,18 @@ urlpatterns = patterns('',
         {'document_root': MEDIA_ROOT}),
     (r'^admin/', include(admin.site.urls)),
 )
+
+MENU_ITEMS = (
+    ('Home', 'main', '/'),
+    ('Monitor', 'monitor', '/pricewatch'),
+    ('Plans', 'plans', '/plans'),
+    ('Register', 'register', '/register/free'),
+)
+context = lambda page: {'MENU_ITEMS': MENU_ITEMS, 'page': page}
+
+urlpatterns += patterns('django.views.generic.simple',
+    (r'^$', 'direct_to_template', {'template': 'main.html', 'extra_context': context('main')}),
+    (r'^pricewatch/?$', 'direct_to_template', {'template': 'pricewatch.html', 'extra_context': context('monitor')}),
+    (r'^plans/?$', 'direct_to_template', {'template': 'plans.html', 'extra_context': context('plans')}),
+    (r'^register/(?P<plan>\w+)/?$', 'direct_to_template', {'template': 'register.html', 'extra_context': context('register')}),
+)
