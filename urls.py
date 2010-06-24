87c91f0b8e555168e11c4ec8d483babf11419e08
Sergey <jade.armadillo@gmail.com>
2010-06-24 16:09:34 +0300

    password reset pages

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.contrib.auth.views import password_reset, password_reset_done, \
+    password_reset_confirm, password_reset_complete
 from settings import *
 admin.autodiscover()
 
@@ -8,6 +10,10 @@ urlpatterns = patterns('',
         {'document_root': MEDIA_ROOT}),
     (r'^admin/', include(admin.site.urls)),
     (r'^accounts/login/', 'monitor.views.signin'),
+    (r'^accounts/reset_form/', password_reset),
+    (r'^accounts/reset_done/', password_reset_done),
+    (r'^accounts/reset/(?P<uidb36>\w+)-(?P<token>.+)', password_reset_confirm),
+    (r'^accounts/reset_complete/', password_reset_complete),
 )
 
 from monitor.urls import urlpatterns as extra
